41802cfa63d9559544f936f2b2057ac74fba4886
falmagne <falmagne@llr.in2p3.fr>
2018-11-30 17:18:44 +0100

    New (hopefully final) config file for Bc pp 2017 MC request

--- BcToJpsiMuNu/Bc_config.py
+++ BcToJpsiMuNu/Bc_config.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
@@ -16,7 +16,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_Bc_2014.pdl'),
             list_forced_decays = cms.vstring('MyBc+',
                                              'MyBc-'),
-            operates_on_particles = cms.vint32(0),
+            operates_on_particles = cms.vint32(541,-541,543,-543),
             use_default_decay = cms.untracked.bool(False),
             user_decay_embedded = cms.vstring(
 """
@@ -36,7 +36,7 @@ Enddecay
 CDecay B_c*-
 
 Decay MyBc+
- 1.000    MyJpsi      mu+   nu_mu        PHOTOS ISGW2;
+ 1.000    MyJpsi      mu+   nu_mu        PHOTOS BC_VMN 2;
 Enddecay
 CDecay MyBc-
 
@@ -52,7 +52,7 @@ End
         ),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CUEP8M1SettingsBlock,
+        pythia8CP5SettingsBlock,
         processParameters = cms.vstring('543:m0 = 6.34000',
                                         '543:tau0 = 0.',
                                         '543:mayDecay = off',
@@ -62,7 +62,7 @@ End
                                         '541:mayDecay = off',
                                         ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+                                    'pythia8CP5Settings',
                                     'processParameters',
                                     )
         )
@@ -70,20 +70,44 @@ End
 
 generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 
-bcgenfilter = cms.EDFilter("PythiaDauVFilter",
-    DaughterIDs = cms.untracked.vint32(-13),
-    MaxEta = cms.untracked.vdouble(2.5),
-    MinEta = cms.untracked.vdouble(-2.5),
-    MinPt = cms.untracked.vdouble(0.6),
-    NumberDaughters = cms.untracked.int32(1),
-    ParticleID = cms.untracked.int32(541),
-    verbose = cms.untracked.int32(0)
+#P cut for the muon from the W from the Bc
+BcmuWFilter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(13),
+    MotherID = cms.untracked.int32(541),
+    MaxEta = cms.untracked.double(2.6),
+    MinEta = cms.untracked.double(-2.6),
+    MinP = cms.untracked.double(2.4),
+    MinPt = cms.untracked.double(0.6),
+    Status = cms.untracked.int32(1)
 )
 
-mumugenfilter = cms.EDFilter("PythiaDauVFilter",
+#P cut for one muon from a Jpsi
+JpsimuFilter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(13),#sign-insensitive
+    MotherID = cms.untracked.int32(443),
+    MaxEta = cms.untracked.double(2.5),
+    MinEta = cms.untracked.double(-2.5),
+    MinP = cms.untracked.double(2.5),
+    MinPt = cms.untracked.double(0.6),
+    Status = cms.untracked.int32(1)
+)
+
+#P cut for at least one mu+mu- pair
+muPairFilter = cms.EDFilter("MCParticlePairFilter",
+    Status = cms.untracked.vint32(1, 1),
+    MinP = cms.untracked.vdouble(2.4, 2.4),
+    MaxEta = cms.untracked.vdouble(2.6, 2.6),
+    MinEta = cms.untracked.vdouble(-2.6, -2.6),
+    ParticleCharge = cms.untracked.int32(-1),#opposite-charge muons
+    ParticleID1 = cms.untracked.vint32(13),
+    ParticleID2 = cms.untracked.vint32(13),
+)
+
+#Check the Bc decay + the Jpsi dimuon cuts (without P)
+BcToJpsiToMumuFilter = cms.EDFilter("PythiaDauVFilter",
     DaughterIDs = cms.untracked.vint32(-13, 13),
-    MaxEta = cms.untracked.vdouble(2.5, 2.5),
-    MinEta = cms.untracked.vdouble(-2.5, -2.5),
+    MaxEta = cms.untracked.vdouble(2.6, 2.6),
+    MinEta = cms.untracked.vdouble(-2.6, -2.6),
     MinPt = cms.untracked.vdouble(0.6, 0.6),
     MotherID = cms.untracked.int32(541),
     NumberDaughters = cms.untracked.int32(2),
@@ -91,4 +115,4 @@ mumugenfilter = cms.EDFilter("PythiaDauVFilter",
     verbose = cms.untracked.int32(0)
 )
 
-ProductionFilterSequence = cms.Sequence(generator*bcgenfilter*mumugenfilter)
+ProductionFilterSequence = cms.Sequence(generator*BcmuWFilter*JpsimuFilter*muPairFilter*BcToJpsiToMumuFilter)
